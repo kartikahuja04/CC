3008df070ade70faa6ab496dfdf8f47e232da026
Kartik Ahuja <kartikahuja04jan@gmail.com>
2026-07-27 10:17:09 +0530

    Time: 3 ms (27.60%) | Memory: 15.3 MB (16.01%) - LeetSync

---
+++ 78-subsets/subsets.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    vector<vector<int>> subsets(vector<int>& nums) {
+        vector<vector<int>>ans;
+        vector<int> arr;
+        allsubset(arr, 0, ans , nums);
+        return ans;
+    }
+    void allsubset(vector<int> &arr, int index , vector<vector<int>>&ans , vector<int> nums){
+        if(index == nums.size()){
+            ans.push_back(arr);
+            return;
+        }
+        arr.push_back(nums[index]);
+        allsubset(arr,index+1,ans,nums);
+        arr.pop_back();
+        allsubset(arr,index+1,ans,nums);
+    }
+};
